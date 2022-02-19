64d9e2bc2c83084f4452e6de3eec22cbce6d1a8c
yuu383 <yuukikokubo.pg@gmail.com>
2022-02-19 17:30:48 +0900

    webpack.config.js

---
+++ webpack.commonjs.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.commonjs.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
